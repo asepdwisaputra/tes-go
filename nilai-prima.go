563361ccb59add170984bce4b0639d81ee44d6d2
asepdwisaputra <akunasep111@gmail.com>
2023-08-25 14:46:26 +0700

    update nilai-prima

--- nilai-prima.go
+++ nilai-prima.go
@@ -7,19 +7,36 @@ func primeNumber(number int) bool {
 		return false
 	}
 
-	for i := 2; i*i <= number; i++ { // 2,3,4,5,6,7
+	//Cara 1
+	// for i := 2; i*i <= number; i++ { // 2,3,4,5,6,7
+	// 	if number%i == 0 {
+	// 		return false
+	// 	}
+	// }
+
+	//Cara 2
+	if number == 2 {
+		return true
+	}
+	if number%2 == 0 {
+		return false
+	}
+	// 5,7 = 35
+	for i := 3; i*i <= number; i += 2 {
 		if number%i == 0 {
 			return false
 		}
 	}
+
+	////////////////////////
 	return true
 }
 
 func main() {
 	fmt.Println(primeNumber(7))
-	// fmt.Println(primeNumber(1000000007)) // true
-	// fmt.Println(primeNumber(13))         // true
-	// fmt.Println(primeNumber(17))         // true
-	// fmt.Println(primeNumber(20))         // false
-	// fmt.Println(primeNumber(35))         // false
+	fmt.Println(primeNumber(1000000007)) // true
+	fmt.Println(primeNumber(13))         // true
+	fmt.Println(primeNumber(17))         // true
+	fmt.Println(primeNumber(20))         // false
+	fmt.Println(primeNumber(35))         // false
 }
